1b9e90fce15b3c2048e252ff3b4a53ddb82ba4dc
Евгений Евгений <ypssyp1@outlook.com>
2020-02-10 20:50:57 +0300

    Добавьте файлы проекта.

---
+++ task04-find-min-max.cs/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task04_find_min_max.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
